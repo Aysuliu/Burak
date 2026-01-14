cdaa57dddf839873c172d2088c11aeff383d260a
Aysulu <bakhievaays@gmail.com>
2026-01-14 11:10:09 +0900

    feat: integration of environmental variables

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+// Architectural pattern backend: MVC, DependencyInjection
+// Architectural pattern frontend: MVP(model view presenter)
+
+// MVC = Model View Controller
+// Design pattern: MiddleWare, Decorator
+
+//MongoDB structure: 
+
+import dotenv from 'dotenv';
+dotenv.config(); // config methodni execute qilish
---
+++ src/train.ts
@@ -0,0 +1,33 @@
+// //MIT task #G
+
+// function getHighestIndex(a) {
+//     let maxIndex = 0;
+
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] > a[maxIndex]) {
+//             maxIndex = i;
+//         }
+//     }
+//     return maxIndex;
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
+// console.log(getHighestIndex([5, 12, 21, 8, 2]));
+// console.log(getHighestIndex([5, 12, 35, 45, 55]));
+
+
+
+//MIT task 
+
+function getPositive(a: number[]): string {
+    const arrayA: number[] = [];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > 0) {
+            arrayA.push(a[i]);
+        }
+    }
+    return arrayA.join('');
+}
+
+console.log(getPositive([1, -4, 2]));
+console.log(getPositive([1, -3, 3, -2, 2]));
