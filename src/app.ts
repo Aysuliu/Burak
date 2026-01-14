9585b088e9447b33025ac3be92d090b6277a87cf
Aysulu <bakhievaays@gmail.com>
2026-01-14 11:40:47 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from "path"; //core package
+
+/** 1-Entrance **/
+const app = express();
+app.use(express.static(path.join(__dirname, "public"))); //middleware pattern
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+/** 2-Sessions **/
+
+/** 3-Views **/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', "ejs");
+
+/** 4-Routers **/
+export default app; //commonjs => module.exports()
