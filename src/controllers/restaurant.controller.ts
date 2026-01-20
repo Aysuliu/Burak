365da8c76339b0c34b940a6a2fb28f9577482f63
Aysulu <bakhievaays@gmail.com>
2026-01-20 22:36:28 +0900

    feat: develop restaurant login process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,7 +3,7 @@ import { Request, response, Response} from 'express';
 import { T } from "../libs/types/common";
 
 import MemberService from "../models/Member.service";
-import { MemberInput } from '../libs/types/member';
+import { LoginInput, MemberInput } from '../libs/types/member';
 import { MemberType } from '../libs/enums/member.enum';
 
 
@@ -37,12 +37,18 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 };
 
 
-restaurantController.processLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: Request, res: Response) => {
     try {
         console.log('processLogin');
-        res.send("Done!");
+        console.log("body:", req.body);
+        const input: LoginInput = req.body;
+
+        const memberService = new MemberService();
+        const result = await memberService.processLogin(input);
+        res.send(result);
     } catch (err) {
         console.log("Error. processLogin:", err);
+        res.send(err);
     }
 };
 
