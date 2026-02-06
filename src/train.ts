bc22d28dd668b0d4a8f5eb465f6760a67dce4efe
Aysulu <bakhievaays@gmail.com>
2026-02-06 15:11:28 +0900

    feat: MIT task O

--- src/train.ts
+++ src/train.ts
@@ -148,17 +148,6 @@
 // console.log(palindromCheck("mom"));
 
 //TASK O
-/** 
- * Shunday function yozing va u har xil qiymatlardan iborat array qabul qilsin.
-Va array ichidagi sonlar yig'indisini hisoblab chiqgan javobni qaytarsin
-
-MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]); return 45
-
-Yuqoridagi misolda array tarkibida faqatgina ikkita yagona son mavjud bular 10 hamda 35
-Qolganlari nested bo'lib yoki type'lari number emas.
-*/
-
-
 function calculateSumOfNumbers(arr: any[]): number {
     let sum = 0;
     
