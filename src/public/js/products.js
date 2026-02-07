461562f3f30d60ad30226f9d155372f5a9c9a1bd
Aysulu <bakhievaays@gmail.com>
2026-02-08 01:31:58 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -11,6 +11,7 @@ $(function() {
             $("#product-collection").show();
         }
     });
+
     $("#process-btn").on("click", () => {
         $(".dish-container").slideToggle(500);
         $("#process-btn").css("display", "none");
@@ -21,7 +22,27 @@ $(function() {
         $("#process-btn").css("display", "flex");
     });
 
+    $(".new-product-status").on("change", async function(e) {
+        const id = e.target.id;
+        const productStatus = $(`#${id}.new-product-status`).val();
+        console.log("id:", id);
+        console.log("productStatus:", productStatus);
 
+        try {
+            const response = await axios.post(`/admin/product/${id}`, {
+                productStatus: productStatus
+            })
+            console.log("response:", response);
+            const result = response.data;
+            if (result.data) {
+                console.log("product updated!");
+                $(".new-product-status").blur();
+            } else alert("Product update failed");
+        } catch (err) {
+            console.log(err);
+            alert("Product update failed");
+        }
+    })
 });
 
 function validateForm() {
@@ -49,7 +70,8 @@ function validateForm() {
 function previewFileHandler(input, order) {
     const imgClassName = input.className;
     console.log("input:", input);
-    const file = $(`.${imgClassName}`).get(0).files[0];
+    const file = $(`.$ { imgClassName }
+                                    `).get(0).files[0];
     const fileType = file["type"];
     const validImageType = ["image/jpg", "image/jpeg", "image/png"];
 
@@ -59,7 +81,9 @@ function previewFileHandler(input, order) {
         if (file) {
             const reader = new FileReader();
             reader.onload = function() {
-                $(`#image-section-${order}`).attr("src", reader.result);
+                $(`#
+                                    image - section - $ { order }
+                                    `).attr("src", reader.result);
             };
             reader.readAsDataURL(file);
         }
