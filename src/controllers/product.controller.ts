1ba037bff3e810600626c349b0393ed323ade20f
Aysulu <bakhievaays@gmail.com>
2026-02-08 01:14:15 +0900

    feat: create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -29,6 +29,7 @@ productController.getAllProducts = async (req: Request, res: Response) => {
 productController.createNewProduct = async (req: AdminRequest, res: Response) => {
     try {
         console.log('createNewProduct');
+        console.log("req.body:", req.body);
         if(!req.files?.length) throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
         const data: ProductInput = req.body;
@@ -37,12 +38,12 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
             //return ele.path; //windows
         });
         await productService.createNewProduct(data);
-        res.send(`<script> alert("Successful Creation!"); window.location.replace('admin/product/all') </script>`);
+        res.send(`<script> alert("Successful Creation!"); window.location.replace('/admin/product/all') </script>`);
 
     } catch (err) {
         console.log("Error, createNewProduct:", err);
         const message = err instanceof Errors ? err.message: Message.SOMETHING_WENT_WRONG;
-        res.send(`<script> alert("${message}"); window.location.replace('admin/product/all') </script>`);
+        res.send(`<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`);
     }
 };
 
