2157fa6bfb6a73a510dcc4b198384721e472f7cb
Aysulu <bakhievaays@gmail.com>
2026-02-08 17:34:55 +0900

    fix: admin project refactoring

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,27 +1,23 @@
 console.log("Users frontend javascript file");
 $(function() {
     $(".member-status").on("change", function(e) {
-        const id = e.target.id;
-        console.log("id:", id);
+        const id = e.target.id,
+            memberStatus = $(`#${id}.member-status`).val();
 
-        const memberStatus = $(`#${id}.member-status`).val();
-        console.log("memberStatus:", memberStatus);
-
-        //TODO Axios updateChosenUser
-        axios.post("/admin/user/edit", {
-            _id: id,
-            memberStatus: memberStatus,
-        }).then(response => {
-            console.log("response:", response);
-            const result = response.data;
-            console.log("result:", result);
-            if (result.data) {
-                console.log("User updated!");
-                $(".member-status").blur();
-            } else alert("USER update failed");
-        }).catch(err => {
-            console.log(err);
-            alert("USER update failed");
-        });
+        // Axios updateChosenUser
+        axios
+            .post("/admin/user/edit", {
+                _id: id,
+                memberStatus: memberStatus,
+            }).then(response => {
+                console.log("response:", response);
+                const result = response.data;
+                if (result.data) {
+                    $(".member-status").blur();
+                } else alert("USER update failed");
+            }).catch(err => {
+                console.log(err);
+                alert("USER update failed");
+            });
     });
 });
